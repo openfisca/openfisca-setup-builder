0df5924c2c5ec030fc6cc5bbaff32ab2268db4da
Mauko Quiroga <public@mauko.me>
2024-10-24 16:19:11 +0200

    ci: make sure shfmt is installed

--- tests/first_time_setup/e2e/test_e2e.sh
+++ tests/first_time_setup/e2e/test_e2e.sh
@@ -1,20 +1,31 @@
 #!/bin/bash
 
-set -euo pipefail
+test::build() {
+  local -r pip_shfmt=$(command -v shfmt 2>/dev/null)
+  if [[ -z ${pip_shfmt} ]]; then pip install shfmt-py 1>/dev/null; fi
+  ./build.sh 1>/dev/null
+}
 
-declare -xr DRY_MODE=true
-declare -xr JURISDICTION_NAME="L'Île-d'Yeu"
-declare -xr REPOSITORY_URL='https://github.com/openfisca/openfisca-ile-d_yeu'
+test::run() {
+  echo -e "$(./first-time-setup.sh 2>&1 1>/dev/null)"
+}
 
-source 'src/first_time_setup/utils/colours.sh'
+main() {
+  set -euo pipefail
+  declare -xr DRY_MODE=true
+  declare -xr JURISDICTION_NAME="L'Île-d'Yeu"
+  declare -xr REPOSITORY_URL='https://github.com/openfisca/openfisca-ile-d_yeu'
+  source 'src/first_time_setup/utils/colours.sh'
+  test::build
+  local -r result=$(test::run)
 
-./build.sh 1>/dev/null
-result=$(./first-time-setup.sh  2>&1 1>/dev/null)
+  if [[ -n ${result} ]]; then
+    colour::fail 'First time setup failed with error:'
+    colour::logs "${result}"
+    exit 1
+  fi
 
-if [[ -n "${result}" ]]; then
-  colour::fail 'First time setup failed with error:'
-  colour::logs "${result}"
-  exit 1
-fi
+  colour::done 'First time setup ran successfully!'
+}
 
-colour::done 'First time setup ran successfully!'
+main "$@"
